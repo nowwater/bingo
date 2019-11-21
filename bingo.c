eb6ae65f21b04f8dbfec30461545906b8e590e0e
hyodonglee <good79797979@naver.com>
2019-11-21 07:06:05 +0000

    title1

--- bingo.c
+++ bingo.c
@@ -12,7 +12,7 @@
 #include<sys/select.h>
 #include <pthread.h>
 #include<ncurses.h>
-#include<ncursesw/curses.h>
+//#include<curses.h>
 
 #define LEFTEDGE 20
 #define RIGHTEDGE 18
@@ -46,16 +46,76 @@ int Board[20][12] = {//첫번째 판 초기화화
 		{1,0,0,0,0,0,0,0,0,0,0,1},
 		{1,1,1,1,1,1,1,1,1,1,1,1}
 };
-void make_board();
+
+
+//function for  title screen
+void title();
+void game_mode(int y, int x);
+void explan_mode(int, int);
+void quit_mode(int, int);
+
 int main()
 {
+	setlocale(LC_CTYPE, "ko_KR.utf-8");
+	stdscr=initscr();
 
+	title();
+
+	return 0;
 }
 
-void make_board()
+void title()
 {
-	stdscr=initscr();
-	//curses 실행
-	
-	
+	int ch;
+	int y,x,i,j;
+	getmaxyx(stdscr, y, x);
+	keypad(stdscr, TRUE);
+	noecho();
+	curs_set(0);
+
+	game_mode(y,x);
+	refresh();
+
+	sleep(5);
+	endwin();
+}
+
+void game_mode(int y, int x)
+{
+	standend();
+	mvprintw(y/2,(x/3)+17,     "*******************************");
+	standout();
+	mvprintw((y/2)+1, (x/3)+17,"           Start Game          ");
+	standend();
+	mvprintw((y/2)+2, (x/3)+17,"           Game  Rule          ");
+	mvprintw((y/2)+3, (x/3)+17,"              Exit             ");
+	mvprintw(y/2,(x/3)+17,     "*******************************");
+	refresh();
+}
+
+void explan_mode(int y, int x)
+{
+	standend();
+	mvprintw(y/2,(x/3)+17,     "*******************************");
+	mvprintw((y/2)+1, (x/3)+17,"           게 임 하 기         ");
+	standout();
+	mvprintw((y/2)+2, (x/3)+17,"           게 임 설 명         ");
+	standend();
+	mvprintw((y/2)+3, (x/3)+17,"           게 임 종 료         ");
+	mvprintw(y/2,(x/3)+17,     "*******************************");
+	refresh();
+}
+
+
+void quit_mode(int y, int x)
+{
+	standend();
+	mvprintw(y/2,(x/3)+17,     "*******************************");
+	mvprintw((y/2)+1, (x/3)+17,"           게 임 하 기         ");
+	mvprintw((y/2)+2, (x/3)+17,"           게 임 설 명         ");
+	standout();
+	mvprintw((y/2)+3, (x/3)+17,"           게 임 종 료         ");
+	standend();
+	mvprintw(y/2,(x/3)+17,     "*******************************");
+	refresh();
 }
