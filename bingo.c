3c2418fb4f1ef707b36650ead84234479960f031
hyodonglee <good79797979@naver.com>
2019-11-21 13:30:02 +0000

    add title

--- bingo.c
+++ bingo.c
@@ -54,6 +54,10 @@ void game_mode(int y, int x);
 void explan_mode(int, int);
 void quit_mode(int, int);
 
+//function for play, explanation, quit
+void play();
+void explan();
+
 int main()
 {
 	setlocale(LC_CTYPE, "ko_KR.utf-8");
@@ -66,17 +70,63 @@ int main()
 
 void title()
 {
-	int ch;
+	int ch, cnt=1;
 	int y,x,i,j;
 	getmaxyx(stdscr, y, x);
 	keypad(stdscr, TRUE);
 	noecho();
+
 	curs_set(0);
-
+	
 	game_mode(y,x);
-	refresh();
+	
+	while(1)
+	{
+		ch = getch();
+		if(ch == KEY_DOWN||ch == KEY_UP)
+		{
+			if(ch == KEY_DOWN)
+			{
+				if(cnt==3)
+					cnt=1;
+				else
+					cnt++;
+			}
+			else
+			{
+				if(cnt==1)
+					cnt=3;
+				else
+					cnt--;
+			}
 
-	sleep(5);
+			if(cnt == 1)
+				game_mode(y,x);
+			else if(cnt == 2)
+				explan_mode(y,x);	
+			else
+				quit_mode(y,x);
+		}
+
+		else if(ch == 10)
+			break;
+		else
+			continue;
+	}
+	//for each case screen
+	
+	switch(cnt)
+	{
+		case 1:
+			play();
+			break;
+		case 2:
+			explan();
+			break;
+		case 3:
+			endwin();
+	}
+
 	endwin();
 }
 
@@ -89,7 +139,7 @@ void game_mode(int y, int x)
 	standend();
 	mvprintw((y/2)+2, (x/3)+17,"           Game  Rule          ");
 	mvprintw((y/2)+3, (x/3)+17,"              Exit             ");
-	mvprintw(y/2,(x/3)+17,     "*******************************");
+	mvprintw((y/2)+4,(x/3)+17, "*******************************");
 	refresh();
 }
 
@@ -97,12 +147,12 @@ void explan_mode(int y, int x)
 {
 	standend();
 	mvprintw(y/2,(x/3)+17,     "*******************************");
-	mvprintw((y/2)+1, (x/3)+17,"           게 임 하 기         ");
+	mvprintw((y/2)+1, (x/3)+17,"           Start Game          ");
 	standout();
-	mvprintw((y/2)+2, (x/3)+17,"           게 임 설 명         ");
+	mvprintw((y/2)+2, (x/3)+17,"           Game  Rule          ");
 	standend();
-	mvprintw((y/2)+3, (x/3)+17,"           게 임 종 료         ");
-	mvprintw(y/2,(x/3)+17,     "*******************************");
+	mvprintw((y/2)+3, (x/3)+17,"              Exit             ");
+	mvprintw((y/2)+4,(x/3)+17, "*******************************");
 	refresh();
 }
 
@@ -111,11 +161,45 @@ void quit_mode(int y, int x)
 {
 	standend();
 	mvprintw(y/2,(x/3)+17,     "*******************************");
-	mvprintw((y/2)+1, (x/3)+17,"           게 임 하 기         ");
-	mvprintw((y/2)+2, (x/3)+17,"           게 임 설 명         ");
+	mvprintw((y/2)+1, (x/3)+17,"           Start Game          ");
+	mvprintw((y/2)+2, (x/3)+17,"           Game  Rule          ");
 	standout();
-	mvprintw((y/2)+3, (x/3)+17,"           게 임 종 료         ");
+	mvprintw((y/2)+3, (x/3)+17,"              Exit             ");
 	standend();
-	mvprintw(y/2,(x/3)+17,     "*******************************");
+	mvprintw((y/2),(x/3)+17,    "*******************************");
 	refresh();
+}
+
+void play()
+{
+}
+
+void explan(int y, int x)
+{
+	int pointx=x/3;
+	int pointy=y/3;
+	int ch;
+
+	clear();
+
+	mvprintw(pointy, pointx,    "This is a BINGO GAME!");
+	mvprintw(pointy+1, pointx, "1. Make a bingo board with pressing a number key." );
+	mvprintw(pointy+2, pointx, "2. Put a number one by one." );
+	mvprintw(pointy+3, pointx, "3. If you make 3 lines first, you WIN." );
+	mvprintw(pointy+4, pointx, "4. Just play it now!");
+	
+	mvprintw(3*y/4, pointx-17, "Go back : q");
+	refresh();
+
+	while(1)
+	{
+		ch = getch();
+
+		if (ch == 'q' || ch == 'Q')
+		{
+			title();
+		}
+
+	}
+	
 }
