8a7770e88e45fb8ef3eb37a9b8f4b6ac41290468
hyodonglee <good79797979@naver.com>
2019-11-30 19:08:35 +0000

    revised server except signal

--- server.c
+++ server.c
@@ -1,10 +1,9 @@
 /* server.c
- * compile: gcc server.c -o server
- * build : ./server 9190
- */
+* compile: gcc server.c -o server -lpthread
+* build : ./server 9190
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -12,40 +11,42 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 #define BUF_SIZE 1024
 
-char* escapechar1 = "q\n";
-char* escapechar2 = "Q\n";
-
 void error_handling(char*);
-int exitCheck(char*, char*, int);
+void* recv_bingo_message(void*);
+void* recv_num_to_erase();
+
 int str_int(char* num);
+int bingo_flag = 1;
+int winner = -1;
+int client_s[3];
+int client_s2[3];
+
+char* startgame_msg = "game start!\n";
+char* call_msg = "call the number!\n";
+char* turn_msg = "TURN\n";
+char* winner_msg = "WINNER\n";
+char* loser_msg = "LOSER\n";
+
+char message[BUF_SIZE];
 
 int main(int argc, char* argv[])
 {
 	//socket address of server and client 
 	int serv_sock;
-	int clnt_sock;
+	int clnt_sock, clnt_bingo_sock;
 	int str_len, i, j;
 	int num = 0;
-	int client_s[5];
-	struct sockaddr_in serv_adr, clnt_adr;
 
-	int nfds;
-	int n;
-	int fdNum;
+	struct sockaddr_in serv_adr, clnt_adr;
+	int n, cnt = 0;
 	int cnt_done = 0;
 	int turn = 0;
-	struct timeval timeout;
-	char num_erase[50], num_send[50];
-
-	char* startgame = "game start!\n";
-	char* startcalling = "call the number!\n";
-	char* turnmessage = "TURN\n";
-	char message[BUF_SIZE];
+	pthread_t number_thread, bingo_thread[3];
 	socklen_t clnt_adr_sz;
-	fd_set read_fds, cpyReads;
 
 	if (argc != 2)
 	{
@@ -69,123 +70,100 @@ int main(int argc, char* argv[])
 		error_handling("bind() error");
 
 	//stage3. listen
-	if (listen(serv_sock, 5) == -1)
+	if (listen(serv_sock, 3) == -1)
 		error_handling("listen() error");
 
-	nfds = serv_sock + 1;
-	//maximum socket number +1
+	//stage4. accept
+	while (num < 3)
+	{
+		clnt_adr_sz = sizeof(clnt_adr);
+		clnt_sock = accept(serv_sock, (struct sockaddr*) & clnt_adr, &clnt_adr_sz);
+		clnt_bingo_sock = accept(serv_sock, (struct sockaddr*) & clnt_adr, &clnt_adr_sz);
 
-	FD_ZERO(&read_fds);
-	FD_SET(serv_sock, &read_fds);
+		client_s[num] = clnt_sock;
+		client_s2[num++] = clnt_bingo_sock;
+		//client descriptor을 배열에 저장
+
+		printf("player %d connected.\n", num);
+	}
+
+	printf("Start bingo game!\n");
+
+	for (j = 0; j < num; j++)
+	{
+		send(client_s[j], startgame_msg, strlen(startgame_msg), 0);
+		//write로 바꾸기&thread 사용하기
+	}
+	memset(message, 0, sizeof(message));
 
 	while (1)
 	{
-		cpyReads = read_fds;
-		timeout.tv_sec = 5;
-		timeout.tv_usec = 5000;
+		for (j = 0; j < num; j++)//receive messages from client
+		{
 
-		//stage3.5 select
-		if ((fdNum = select(nfds, &cpyReads, 0, 0, NULL)) == -1)
+			if ((n = read(client_s[j], message, BUF_SIZE)) == -1)
+				error_handling("everybody done error");
+
+			message[n] = '\0';
+			printf("message received from player %d : %s", j + 1, message);
+			if (!strcmp(message, "DONE!\n"))
+				cnt_done++;
+			if (cnt_done >= 3)
+				break;
+		}
+		if (cnt_done >= 3) break;
+	}
+	//all users ended filling their own boards.
+
+	printf("All done!\n");
+
+	//tell the clients that all players are ready
+	for (j = 0; j < num; j++)
+	{
+		if (n = write(client_s[j], call_msg, strlen(call_msg)) == -1)
+			error_handling("ready error");
+	}
+
+	/* Start to input the number to erase */
+	pthread_create(&number_thread, NULL, recv_num_to_erase, NULL);
+	for (i = 0; i < 3; i++)
+		pthread_create(&bingo_thread[i], NULL, recv_bingo_message, (void*)& client_s2[i]);
+
+	printf("Create Done in a given time\n");
+	i = 0;
+	while (1)
+	{
+		if (bingo_flag == 0)
+		{
+			printf("client_s2[%d] : %d\n", i, client_s2[i]);
+			if (i == (winner / 2) - 2)
+			{
+				if ((n = write(client_s2[i], winner_msg, strlen(winner_msg))) == -1)
+					error_handling("write error()");
+				printf("%d WINNER\n", client_s2[i]);
+			}
+			else
+			{
+				if ((n = write(client_s2[i], loser_msg, strlen(loser_msg))) == -1)
+					error_handling("write error()");
+				printf("%d LOSER\n", client_s2[i]);
+			}
+			i++;
+			cnt++;
+		}
+		if (cnt == 3)
 			break;
-		if (fdNum == 0)
-			continue;
+	}
+	pthread_join(number_thread, NULL);
+	printf("number_thread_join Done\n");
 
-		while (num < 3)
-		{
-			if (FD_ISSET(serv_sock, &cpyReads))
-			{
-				//connection request!
-				clnt_adr_sz = sizeof(clnt_adr);
-				clnt_sock = accept(serv_sock, (struct sockaddr*) & clnt_adr, &clnt_adr_sz);
-				FD_SET(clnt_sock, &read_fds);
+	for (i = 0; i < 3; i++)
+		pthread_join(bingo_thread[i], NULL);
+	printf("bingo_thread_join Done\n");
 
-				//new client socket
-				if (nfds < clnt_sock)
-					nfds = clnt_sock;
-
-				client_s[num++] = clnt_sock;
-				//client descriptor을 배열에 저장
-
-				printf("%dth player connected.\n", num);
-
-			}
-		}
-
-		printf("start bingo game!\n");
-
-		for (j = 0; j < num; j++)
-		{
-			write(client_s[j], startgame, strlen(startgame));
-			//write로 바꾸기&thread 사용하기
-		}
-		memset(message, 0, sizeof(message));
-
-		while (1)
-		{
-			for (j = 0; j < num; j++)//receive messages from client
-			{
-				if (FD_ISSET(client_s[j], &read_fds))
-				{
-					if ((n = recv(client_s[j], message, BUF_SIZE, 0)) > 0)
-					{
-						message[n] = '\0';
-						printf("message received from %dth player:%s\n", j + 1, message);
-						if (strcmp(message, "DONE!\n") == 0)
-							cnt_done++;
-						if (cnt_done >= 3)
-							break;
-					}
-				}
-			}
-			if (cnt_done >= 3) break;
-		}
-
-
-		//all users ended filling their own boards.
-
-		printf("all done\n");
-
-		/************************************************************************/
-
-		for (j = 0; j < num; j++)
-			//tell the clients that all players are ready
-		{
-			send(client_s[j], startcalling, strlen(startcalling), 0);
-			//sleep(3);
-		}
-
-
-		while (1)
-		{
-			if (turn > 2)
-				turn = 0; //turn: 0,1,2,0,1,2...
-
-			if ((n = write(client_s[turn], turnmessage, strlen(turnmessage))) > 0)
-			{
-				printf("%dth player's turn-sent the signal.\n", turn + 1);
-			}
-			
-			if ((n = read(client_s[turn], num_erase, strlen(num_erase))) == -1)
-				error_handling("number to erase error");
-
-			
-			for (j = 0; j < num; j++)
-			{
-				if ((n = write(client_s[j], num_erase, strlen(num_erase))) > 0)
-				{
-					printf("player %d's number was sent.\n", turn + 1);
-				}
-			}
-
-			turn++;
-		}
-
-	}//while(1)
-
-
-
-}//main
-//
+	close(clnt_sock);
+	close(clnt_bingo_sock);
+}
 
 int str_int(char* num)
 {
@@ -203,33 +181,68 @@ int str_int(char* num)
 	return number;
 }
 
-
 void error_handling(char* message)
 {
-
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
 }
 
-int exitCheck(char* rline, char* escape, int len)
+void* recv_bingo_message(void* client)
 {
-	int i, max;
-	char* tmp;
-
-	max = strlen(rline);
-	tmp = rline;
-	for (i = 0; i < max; i++)
+	int n;
+	int* clnt_num = client;
+	char temp_msg[10];
+	while ((n = read(*clnt_num, temp_msg, BUF_SIZE)) != -1)
 	{
-		if (*tmp == escape[0])
+		temp_msg[n] = '\0';
+		printf("%d said %s!\n", *clnt_num, temp_msg);
+		if (bingo_flag)
 		{
-			if (strncmp(tmp, escape, len) == 0)
-				return 1;
+			winner = *clnt_num;
+			bingo_flag = 0;
+			printf("bingo flag changed : %d\n", bingo_flag);
+			printf("Winner : %d\n", winner);
+			break;
 		}
-		else
-			tmp++;
 	}
-	return -1;
+	if (n < 0)
+		error_handling("read error()");
 
+	return NULL;
 }
 
+void* recv_num_to_erase()
+{
+	int turn = 0, n;
+	char temp_msg[10];
+	while (1)
+	{
+		if (turn > 2)
+			turn = 0;
+
+		if ((n = write(client_s[turn], turn_msg, strlen(turn_msg))) > 0)
+		{
+			//tell the turn to that user
+			printf("player%d's turn sent the signal.\n", turn + 1);
+		}
+
+		// socket + thread
+		if ((n = recv(client_s[turn], temp_msg, BUF_SIZE, 0)) == -1)
+			error_handling("recv_num_error()");
+
+		temp_msg[n] = '\0';
+		//got someting(num from the turn user)?
+		printf("number received from %dth player: %s\n", turn + 1, temp_msg);
+
+		//send it to all client..
+		for (int j = 0; j < 3; j++)
+		{
+			if (j == turn) continue;
+			if ((n = write(client_s[j], temp_msg, strlen(temp_msg))) > 0)
+				printf("sent %s to %d client completed\n", temp_msg, j+1);
+		}
+		turn++;
+	}
+	return NULL;
+}
